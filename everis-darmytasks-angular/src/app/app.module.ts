1fd622845fb088c255ebb8bbda12c4301ad13562
aanibarr <aanibarr@host.docker.internal>
2020-12-22 13:58:31 +0100

    Interfaz bien

--- everis-darmytasks-angular/src/app/app.module.ts
+++ everis-darmytasks-angular/src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import {TasksService} from './services/tasks/api/tasks.service'
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
-
-import appRoutes from './routerConfig';
 
 @NgModule({
   declarations: [
@@ -15,8 +23,17 @@ import appRoutes from './routerConfig';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    FormsModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- everis-darmytasks-angular/src/app/task-list/task-list.component.ts
+++ everis-darmytasks-angular/src/app/task-list/task-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Task } from '../taskModel';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import {Task} from '../taskModel';
+
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+//import { TasksService } from 'src/app/services/tasks';
+
 
 @Component({
   selector: 'app-task-list',
@@ -8,30 +16,121 @@ import { Task } from '../taskModel';
 })
 export class TaskListComponent implements OnInit {
 
-  constructor() {
-  }
-
-  public taskList: Task [] = [
-    {id: 1, description: 'Tarea numero uno', status: 'Completada'},
-    {id: 2, description: 'Tarea numero dos', status: 'En proceso'},
-    {id: 3, description: 'Tarea numero tres', status: 'Pendiente'},
-    {id: 35, description: 'Tarea numero cuatro', status: 'Pendiente'}
+  @ViewChild('dialog', {static: false})
+  public dialog: TemplateRef<any>;
+  taskStatusOpts = [
+    {
+      value: 'inProgress',
+      label: 'In progress'
+    },
+    {
+      value: 'pending',
+      label: 'Pending'
+    },
+    {
+      value: 'finished',
+      label: 'Finished'
+    }
   ];
 
-  ngOnInit() {
+  tasks: Task[];
 
-    console.log('Lista: ', this.taskList);
+  newTaskData = {
+    show: false,
+    control: new FormControl('')
+  };
+
+  deleteTaskData = {
+    index: null,
+    dialog: null
+  };
+  
+  constructor(private matDialog: MatDialog, private _snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    //TODO: Get tasks
+    // this.tasks = [];
+    // for (let i = 0; i < 10; i++) {
+    //   this.tasks.push({ id: i, description: 'Description of Task ' + i, status: this.taskStatusOpts[(i%3)].value});
+    // }
+    this.tasks = [
+    {id: 1, description: 'Tarea numero uno', status: this.taskStatusOpts[(1%3)].value},
+    {id: 2, description: 'Tarea numero dos', status: this.taskStatusOpts[(2%3)].value},
+    {id: 3, description: 'Tarea numero tres', status: this.taskStatusOpts[(3%3)].value},
+  ];
 
   }
 
-  addTask() {
-    this.taskList.push(new Task());
+  public newTask() {
+    this.newTaskData.show = true;
+    this.newTaskData.control.setValue('');
   }
 
-  removeTask(index: number) {
-    if (index > -1) {
-      this.taskList.splice(index, 1);
+  public createTask() {
+    const newTask = {
+      id: this.tasks.length,
+      status: 'pending',
+      description: this.newTaskData.control.value
     }
+    console.log('Creating task', newTask);
+    //TODO: Add task
+    this.newTaskData.show = false;
+    this.newTaskData.control.setValue('');
+    this.tasks.push(newTask);
+    this.openSnackBar('Task created successfully','');
+    console.log('Tarea creada con exito');
+  }
+
+  public cancelNewTask() {
+    this.newTaskData.show = false;
+    this.newTaskData.control.setValue('');
+  }
+
+  public updateTask(idx) {
+    const taskToUpdate = this.tasks[idx];
+    if (taskToUpdate) {
+      console.log('Updating task', taskToUpdate);
+      //TODO Update Task
+      this.openSnackBar('Task with id ' + taskToUpdate.id + ' updated successfully','');
+    }
+  }
+
+  public deleteTask(idx) {
+    this.deleteTaskData.index = idx;
+    this.deleteTaskData.dialog = this.matDialog.open(this.dialog);
+  }
+
+  public deleteConfirm() {
+    const taskToDelete = this.tasks[this.deleteTaskData.index];
+    if (taskToDelete) {
+      console.log('Deleting task', taskToDelete);
+      //TODO: Delete Task
+      // get index of object with id:37
+      var removeIndex = this.tasks.map(function(item) { return item.id; }).indexOf(this.deleteTaskData.index);
+      // remove object
+      this.tasks.splice(removeIndex, 1);
+
+      this.openSnackBar('Task with id ' + taskToDelete.id + ' removed successfully','');
+    }
+    this.deleteTaskData.dialog.close();
+    this.deleteTaskData.index = null;
+    this.deleteTaskData.dialog = null;
+  }
+
+  public deleteCancel() {
+    this.deleteTaskData.dialog.close();
+    this.deleteTaskData.index = null;
+    this.deleteTaskData.dialog = null;
+  }
+
+  private getTasks() {
+    //TODO getTasks
+  }
+
+  public openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
 }
