c3a04641c03369211efe415cde6bb6ae62ded466
aanibarr <aanibarr@host.docker.internal>
2020-12-22 13:58:46 +0100

    cambios

--- everis-darmytasks-angular/src/app/routerConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskListComponent } from './task-list/task-list.component';
-
-export const appRoutes: Routes = [
-{ path: '', pathMatch: 'full', redirectTo: '/listaTareas' },
-{ path: 'listaTareas', component: TaskListComponent}
-];
-
-export default appRoutes;
